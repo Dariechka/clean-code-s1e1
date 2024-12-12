d5ffaa7905c10f39b82fdce05875657a0d48c4f5
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2024-12-12 15:38:13 +0100

    refactor: change class and id names according to rule 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-    listItem.classList.add("list__item");
+    listItem.classList.add("tasks-list__item");
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
